50dbfd5c319306492f4363178d3f1f753342ab0d
Marah N <marah.n35@gmail.com>
2026-08-31 16:28:12 +0300

    added optimized codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,3 @@
-// import 'package:_5000_image_loading/screen/photo_list_screen.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-// void main() async {
-//   runApp(
-//     const ProviderScope(
-//       child: MyApp(),
-//     ),
-//   );
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       //showPerformanceOverlay: true,
-
-//       home:PhotoListScreen(),
-//     );
-//   }
-// }
-
-
 import 'package:_5000_image_loading/performance_monitor.dart';
 import 'package:_5000_image_loading/provider/image_repository_provider.dart';
 import 'package:_5000_image_loading/screen/bad_screen.dart';
@@ -100,7 +73,7 @@ class BadExperimentHome extends ConsumerWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
-                        builder: (_) => BadScreen(
+                        builder: (_) => OptimizedScreen(
                           screenNumber: index + 1,
                           photos: photos,
                         ),
